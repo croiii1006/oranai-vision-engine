bc31d6642a0c53b49a5c14ffab33e02a8666294a
jason828lee <youquanli835@gmail.com>
2026-02-26 23:14:31 +0800

    -

--- vite.config.ts
+++ vite.config.ts
@@ -32,16 +32,16 @@ export default defineConfig(({ mode }) => ({
       },
       // AUTH 相关 API 代理 - 代理到开发服务器
       '/auth': {
-        // target: 'http://94.74.101.163:28080',
-        target: 'http://192.168.112.253:8000/',
+        target: 'http://94.74.101.163:28080',
+        // target: 'http://192.168.112.253:8000/',
         changeOrigin: true,
         secure: false, // 如果是 http，设置为 false
         ws: true, // 支持 WebSocket
       },
       // OAuth 相关 API 代理（/oauth/callback/google 由前端 SPA 处理，不转发到后端）
       '/oauth': {
-        // target: 'http://94.74.101.163:28080',
-        target: 'http://192.168.112.253:8000/',
+        target: 'http://94.74.101.163:28080',
+        // target: 'http://192.168.112.253:8000/',
         changeOrigin: true,
         secure: false,
         bypass(req) {
@@ -52,22 +52,22 @@ export default defineConfig(({ mode }) => ({
         },
       },
       '/oauth2': {
-        // target: 'http://94.74.101.163:28080',
-        target: 'http://192.168.112.253:8000/',
+        target: 'http://94.74.101.163:28080',
+        // target: 'http://192.168.112.253:8000/',
         changeOrigin: true,
         secure: false,
       },
       // 验证码 API 代理
       '/api/captcha': {
-        // target: 'http://94.74.101.163:28080',
-        target: 'http://192.168.112.253:8000/',
+        target: 'http://94.74.101.163:28080',
+        // target: 'http://192.168.112.253:8000/',
         changeOrigin: true,
         secure: false,
       },
       // 注册 API 代理
       '/api/register': {
-        // target: 'http://94.74.101.163:28080',
-        target: 'http://192.168.112.253:8000/',
+        target: 'http://94.74.101.163:28080',
+        // target: 'http://192.168.112.253:8000/',
         changeOrigin: true,
         secure: false,
       },
